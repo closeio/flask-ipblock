8072dc6f4cedb6a750dadc5fdc4e890c11ae1102
Anthony Nemitz <anemitz@gmail.com>
2014-12-26 12:49:14 -0800

    adding whitelist to indexed fields.

--- flask_ipblock/documents.py
+++ flask_ipblock/documents.py
@@ -11,7 +11,7 @@ class IPNetwork(Document):
     whitelist = BooleanField(default=False)
 
     meta = {
-        'indexes': [('start', 'stop')]
+        'indexes': [('start', 'stop', 'whitelist')]
     }
 
     @classmethod

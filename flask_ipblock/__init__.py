8ad38b0c5184b3f932f0f2b084272c205dbe95f4
Phil Freo <phil@philfreo.com>
2015-10-06 16:40:57 -0400

    Don't do the IP check for requests to static file paths

--- flask_ipblock/__init__.py
+++ flask_ipblock/__init__.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, url_for
 from flask.ext.ipblock.documents import IPNetwork
 
 class IPBlock(object):
@@ -6,10 +6,22 @@ class IPBlock(object):
         app.before_request(self.block_before)
 
     def block_before(self):
+
+        # To avoid unnecessary database queries, ignore the IP check for
+        # requests for static files
+        if request.path.startswith(url_for('static', filename='')):
+            return
+        # Some static files are served from the root path
+        ignored_extensions = ('ico', 'png', 'txt', 'xml')
+        if request.path.rsplit('.', 1)[-1] in ignored_extensions:
+            return
+
         ips = request.headers.getlist("X-Forwarded-For")
-        if ips:
-            ip = ips[0].strip()
-            if ip[-1] == ',':
-                ip = ip[:-1]
-            if IPNetwork.matches_ip(ip.rsplit(',', 1)[-1].strip()):
-                return '', 200
+        if not ips:
+            return
+
+        ip = ips[0].strip()
+        if ip[-1] == ',':
+            ip = ip[:-1]
+        if IPNetwork.matches_ip(ip.rsplit(',', 1)[-1].strip()):
+            return '', 200

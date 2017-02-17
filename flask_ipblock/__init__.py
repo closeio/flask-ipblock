b2f9c61fa5438d8cf16b486693623e279ad5042f
Stefan Wojcik <wojcikstefan@gmail.com>
2017-02-17 14:02:20 -0500

    cleanup

--- flask_ipblock/__init__.py
+++ flask_ipblock/__init__.py
@@ -1,33 +1,42 @@
 from flask import request, url_for
-from flask.ext.ipblock.documents import IPNetwork
+from flask_ipblock.documents import IPNetwork
 
 
 class IPBlock(object):
     def __init__(self, app, read_preference=None):
         """
-        Initialize IPBlock and set up a before_request handler in the app.
+        Initialize IPBlock and set up a before_request handler in the
+        app.
 
-        You can override the default MongoDB read preference via the optional
-        read_preference kwargs.
+        You can override the default MongoDB read preference via the
+        optional read_preference kwarg.
         """
         self.read_preference = read_preference
         app.before_request(self.block_before)
 
     def block_before(self):
-
+        """
+        Check the current request and block it if the IP address it's
+        coming from is blacklisted.
+        """
         # To avoid unnecessary database queries, ignore the IP check for
         # requests for static files
         if request.path.startswith(url_for('static', filename='')):
             return
-        # Some static files are served from the root path
+
+        # Some static files might be served from the root path (e.g.
+        # favicon.ico, robots.txt, etc.). Ignore the IP check for most
+        # common extensions of those files.
         ignored_extensions = ('ico', 'png', 'txt', 'xml')
         if request.path.rsplit('.', 1)[-1] in ignored_extensions:
             return
 
-        ips = request.headers.getlist("X-Forwarded-For")
+        ips = request.headers.getlist('X-Forwarded-For')
         if not ips:
             return
 
+        # If the X-Forwarded-For header contains multiple comma-separated
+        # IP addresses, we're only interested in the last one.
         ip = ips[0].strip()
         if ip[-1] == ',':
             ip = ip[:-1]
